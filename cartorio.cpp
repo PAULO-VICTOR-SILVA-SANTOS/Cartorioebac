42873a03af3cd325cef957e27e5931c8ca50a9b3
PAULO-VICTOR-SILVA-SANTOS <paulinhovictor852@gmail.com>
2024-05-13 22:12:13 -0300

    Opção de saída
    Incluso no sistema a opção de saída do usuário

--- cartorio.cpp
+++ cartorio.cpp
@@ -133,6 +133,7 @@ int main()
 	printf("\t1 - Registrar nomes\n");
 	printf("\t2 - Consutar nomes\n");
 	printf("\t3 - Deletar nomes\n\n"); 	
+	printf("\t4 - sair do sistema\n\n");
 	printf("opcao:");// Fim do menu
 		
 	scanf("%d",  &opcao); // Armazenando a escolha do usuário
@@ -152,6 +153,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sisema!\n");
+			return 0;
 			break;
 		
 			default:
